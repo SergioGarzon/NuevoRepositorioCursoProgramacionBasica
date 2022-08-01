64f95f55a7a5cee133fc9d107c4d63f1f4cce2dd
Sergio Garzon <SergioGarzon@users.noreply.github.com>
2022-08-01 14:41:23 -0300

    Primer commit

---
+++ src/nuevoproyecto/NuevoProyecto.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nuevoproyecto;
+
+/**
+ *
+ * @author Notebook
+ */
+public class NuevoProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int numero = 10;
+        
+        System.out.println(numero);
+        
+    }
+    
+}
